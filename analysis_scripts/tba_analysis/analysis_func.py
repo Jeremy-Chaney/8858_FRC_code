7ffa13c8f2225f035b1ecc261433ff7dd3526e5e
Jeremy Chaney <jeremy.chaney2020@gmail.com>
2025-04-13 23:26:13 -0500

    Adding functions to analyze district points from TBA

--- analysis_scripts/tba_analysis/analysis_func.py
+++ analysis_scripts/tba_analysis/analysis_func.py
@@ -6,7 +6,10 @@
 import requests
 import os
 import sys
+import datetime
 from pathlib import Path
+
+dp_event_data = []
 
 # Handle the API Key for The Blue Alliance
 try:
@@ -84,6 +87,44 @@ def validate_team(team_endpoint):
         print("Unable to validate team, exiting...")
         exit()
 
+def get_team_district(team_num, year):
+    """
+    Return the district code for a team in a given year
+    """
+    url = f"{BASE_URL}/team/frc{team_num}/districts"
+
+    year = int(year)
+
+    response = requests.get(url, headers=headers)
+    districts = response.json()
+
+    for d in districts:
+        if year == d["year"]:
+            print(d)
+            return d["abbreviation"]
+
+    print(f"ERROR : Couldn't find district for {team_num} in year {year}")
+    return "no_district_found"
+
+def get_team_districtname(team_num, year):
+    """
+    Return the district code for a team in a given year
+    """
+    url = f"{BASE_URL}/team/frc{team_num}/districts"
+
+    year = int(year)
+
+    response = requests.get(url, headers=headers)
+    districts = response.json()
+
+    for d in districts:
+        if year == d["year"]:
+            print(d)
+            return d["display_name"]
+
+    print(f"ERROR : Couldn't find district for {team_num} in year {year}")
+    return "no_district_found"
+
 def get_event_id_list(team_events_endpoint, evnum):
     """
     get list of events a team participated in for a given year
@@ -117,6 +158,13 @@ def get_event_name(event_key):
     if response.status_code == 200:
         event_data = response.json()
         return event_data['name']
+
+def get_event_week(event_key):
+    event_endpoint = f"/event/{event_key}"
+    response = requests.get(BASE_URL + event_endpoint, headers=headers)
+    if response.status_code == 200:
+        event_data = response.json()
+        return event_data['week']
 
 def get_event_year(event_key):
     event_endpoint = f"/event/{event_key}"
@@ -162,3 +210,138 @@ def get_event_matches(event_endpoint, team_num):
         print(f"Failed to fetch matches: {response.status_code} - {response.reason}")
         print(f"The event's matches list {matches_endpoint} could not be found, exiting...")
         exit()
+
+def count_event_dp(team_num: str, event_key: str):
+    """
+    Fetches the district points earned at a given event for a given team
+    """
+    url = f"{BASE_URL}/event/{event_key}/district_points"
+    if debug_mode:
+        print(url)
+        get_event_details(event_key=event_key)
+
+    global dp_event_data
+
+    week = 0
+    team_points = 0
+    qual_points = 0
+    elim_points = 0
+    alliance_points = 0
+    award_points = 0
+
+    found_flag = False
+
+    for events in dp_event_data:
+        if events[0] == url:
+            found_flag = True
+            week = events[1]
+
+            try:
+                team_points = events[2]['points'][f'frc{team_num}']['total']
+                qual_points = events[2]['points'][f'frc{team_num}']['qual_points']
+                elim_points = events[2]['points'][f'frc{team_num}']['elim_points']
+                alliance_points = events[2]['points'][f'frc{team_num}']['alliance_points']
+                award_points = events[2]['points'][f'frc{team_num}']['award_points']
+                if team_points:
+                    print(f"District points for {team_num} @ {get_event_name(event_key)} (week {week}): {team_points}")
+                else:
+                    print(f"No district points found for team {team_num} at event {get_event_name(event_key)}")
+            except KeyError:
+                print(f"INFO : No District Point information for {team_num} @ {get_event_name(event_key)}")
+
+    # if this event hasn't already been saved to memory (via dp_event_data), save it here
+    if not found_flag:
+        response = requests.get(url, headers=headers)
+
+        try:
+            if response.status_code == 200:
+                data = response.json()
+                week = get_event_week(event_key)
+                team_points = data['points'][f'frc{team_num}']['total']
+                qual_points = data['points'][f'frc{team_num}']['qual_points']
+                elim_points = data['points'][f'frc{team_num}']['elim_points']
+                alliance_points = data['points'][f'frc{team_num}']['alliance_points']
+                award_points = data['points'][f'frc{team_num}']['award_points']
+                if team_points:
+                    print(f"District points for {team_num} @ {get_event_name(event_key)} (week {week}): {team_points}")
+                else:
+                    print(f"No district points found for team {team_num} at event {get_event_name(event_key)}")
+
+            else:
+                print(f"Failed to fetch data: {response.status_code} - {response.reason}")
+                print("Unable to detect event list for this team/year combination, exiting...")
+                exit()
+        except KeyError:
+            print(f"INFO : No District Point information for {team_num} @ {get_event_name(event_key)}")
+
+        # save this data in memory so it can be accessed quicker later!
+        dp_event_data.append([url, week, data])
+
+    return week, qual_points, elim_points, alliance_points, award_points, team_points
+
+def count_team_dp(team_num, team_events:str):
+    """
+    Calculates the # of district points for a given team
+    """
+    event_ids = []
+    district_pts_by_week = []
+    district_pts = 0
+
+    response = requests.get(team_events, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        # sort list by chronological order
+        team_event_list = [
+            e for e in data
+        ]
+        team_event_list.sort(key=lambda e: datetime.datetime.fromisoformat(e['start_date']))
+
+        district_ev_cnt = 0
+        for event in team_event_list:
+            valid_event = True
+            if event['event_type'] == 1:
+                district_ev_cnt = district_ev_cnt + 1
+                if district_ev_cnt > 2:
+                    valid_event = False
+            elif event['event_type'] == 0:
+                valid_event = False
+
+            event_key = event['key']
+            week, qual_points, elim_points, alliance_points, award_points, team_points = count_event_dp(team_num, event_key)
+
+            # only first two district events cout towards qualification for District Championship
+            if valid_event:
+                district_pts = district_pts + award_points
+                district_pts = district_pts + qual_points
+                district_pts = district_pts + elim_points
+                district_pts = district_pts + alliance_points
+
+            if(week != None):
+                district_pts_by_week.append([week, district_pts])
+    else:
+        print(f"Failed to fetch data: {response.status_code} - {response.reason}")
+        print("Unable to detect event list for this team/year combination, exiting...")
+        exit()
+
+    return district_pts, district_pts_by_week
+
+def get_district_pts_by_week(district_key, year):
+    """
+    Fetches a full district's points by week
+    """
+    teamlist_url = f"{BASE_URL}/district/{year}{district_key}/teams"
+    response = requests.get(teamlist_url, headers=headers)
+    full_dist_pts_by_week = []
+
+    if response.status_code == 200:
+        teams = response.json()
+        for team in teams:
+            team_num = team['key'].replace("frc", "")
+            team_endpoint = f"{BASE_URL}/team/frc{team_num}"
+            team_events = f"{team_endpoint}/events/{year}"
+            district_pts, district_pts_by_week = count_team_dp(team_num, team_events)
+            full_dist_pts_by_week.append([team_num, district_pts, district_pts_by_week])
+
+    return full_dist_pts_by_week
